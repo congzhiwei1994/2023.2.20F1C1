47b4c8ea050013155b700f72acadb5c0e86c75c9
congzhiwei1994 <75684067+congzhiwei1994@users.noreply.github.com>
2024-12-12 03:28:06 +0800

    1

--- 2023.2.20F1C1/Assets/Scripts/Controller/GameController.cs
+++ 2023.2.20F1C1/Assets/Scripts/Controller/GameController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
@@ -5,6 +6,18 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public Camera camera;
+    private MouseOrbit mouseOrbit;
+
+    private void Awake()
+    {
+        if (camera == null)
+        {
+            camera = Camera.main;
+        }
+        mouseOrbit = camera.gameObject.GetComponent<MouseOrbit>();
+    }
+
     public void ExitGame()
     {
 #if UNITY_EDITOR
@@ -12,5 +25,25 @@ public class GameController : MonoBehaviour
 #else
   Application.Quit();
 #endif
+    }
+
+    public void SetHeadCamera()
+    {
+        // mouseOrbit.distance = 1.5f;
+        // mouseOrbit.minDistance = 1.5f;
+        // mouseOrbit.maxDistance = 1.5f;
+        camera.gameObject.GetComponent<MouseOrbit>().distance = 1.5f;
+        camera.gameObject.GetComponent<MouseOrbit>().minDistance = 1.5f;
+        camera.gameObject.GetComponent<MouseOrbit>().maxDistance = 1.5f;
+    }
+    
+    public void SetFullBodyHeadCamera()
+    {
+        // mouseOrbit.distance = 1.5f;
+        // mouseOrbit.minDistance = 1.5f;
+        // mouseOrbit.maxDistance = 1.5f;
+        camera.gameObject.GetComponent<MouseOrbit>().distance = 7f;
+        camera.gameObject.GetComponent<MouseOrbit>().minDistance = 7f;
+        camera.gameObject.GetComponent<MouseOrbit>().maxDistance = 7f;
     }
 }
